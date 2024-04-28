a0546bdf1c54b3a1a49f441b738624ba2d6a2fc1
Wojciech Andrzejuk <wojciech.andrzejuk@wp.pl>
2024-04-28 14:37:30 +0200

    Add body of function

--- main.py
+++ main.py
@@ -2,4 +2,24 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def calculator():
-    pass
+  działanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie: "))
+  a = float(input("Podaj pierwszy składnik: "))
+  b = float(input("Podaj drugi składnik: "))
+  if działanie == 1:
+    dodawanie = a + b
+    logging.info(f"Dodaję {a} i {b}.")
+    print(f"Wynik to: {dodawanie}")
+  elif działanie == 2:
+    odejmowanie = a - b
+    logging.info(f"Odejmuję {b} od {a}.")
+    print(f"Wynik to: {odejmowanie}")
+  elif działanie == 3:
+    mnożenie = a * b
+    logging.info(f"Mnożę {a} i {b}")
+    print(f"Wynik to: {mnożenie}")
+  elif działanie == 4:
+    dzielenie = a / b
+    logging.info(f"Dzielę {a} przez {b}")
+    print(f"Wynik to: {dzielenie}")
+
+calculator()
