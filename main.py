51392db9ec86acbec1176a62ee819203a3623423
Wojciech Andrzejuk <wojciech.andrzejuk@wp.pl>
2024-04-28 14:52:09 +0200

    Update body of function

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ The function works like a simple calculator, performing addition, subtraction, m
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+
 def calculator():
   działanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie: "))
-  a = float(input("Podaj pierwszy składnik: "))
-  b = float(input("Podaj drugi składnik: "))
-  if działanie == 1:
-    dodawanie = a + b
-    logging.info(f"Dodaję {a} i {b}.")
-    print(f"Wynik to: {dodawanie}")
-  elif działanie == 2:
-    odejmowanie = a - b
-    logging.info(f"Odejmuję {b} od {a}.")
-    print(f"Wynik to: {odejmowanie}")
-  elif działanie == 3:
-    mnożenie = a * b
-    logging.info(f"Mnożę {a} i {b}")
-    print(f"Wynik to: {mnożenie}")
-  elif działanie == 4:
-    dzielenie = a / b
-    logging.info(f"Dzielę {a} przez {b}")
-    print(f"Wynik to: {dzielenie}")
+  if 0 < działanie < 5:
+    a = float(input("Podaj pierwszy składnik: "))
+    b = float(input("Podaj drugi składnik: "))
+    if działanie == 1:
+      dodawanie = a + b
+      logging.info(f"Dodaję {a} i {b}.")
+      print(f"Wynik to: {dodawanie}")
+    elif działanie == 2:
+      odejmowanie = a - b
+      logging.info(f"Odejmuję {b} od {a}.")
+      print(f"Wynik to: {odejmowanie}")
+    elif działanie == 3:
+      mnożenie = a * b
+      logging.info(f"Mnożę {a} i {b}")
+      print(f"Wynik to: {mnożenie}")
+    elif działanie == 4:
+      dzielenie = a / b
+      logging.info(f"Dzielę {a} przez {b}")
+      print(f"Wynik to: {dzielenie}")
+  else:
+    print("Podaj liczbę od 1 do 4!!!")
+    calculator()
 
 calculator()
